56cf42e4953997aa3578d57b705ec6c772cdb832
anirbanlife-alt <anirban4life@gmail.com>
2026-08-28 03:14:54 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
    from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 if __name__ == "__main__":
     print("Anirban Ray Choudhury")
     print(date.today())
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", subtract(5, 3))
+    print("5 * 3 =", multiply(5, 3))
